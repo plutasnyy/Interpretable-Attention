843a8ec53f7341f2799439dc00d5a6fbc06371a3
plutasnyy <kamil.plucinski97@gmail.com>
2020-12-03 00:21:24 +0100

    add predixcting spans

--- predict_last_model.py
+++ predict_last_model.py
@@ -1,10 +1,18 @@
 import argparse
 import os
 
+from nltk import word_tokenize, TreebankWordTokenizer
+from nltk.tokenize.api import StringTokenizer
+from termcolor import colored
+
 from Trainers.DatasetBC import datasets
 from Trainers.TrainerBC import Evaluator
 from common_code.common import get_latest_model
 from configurations import configurations
+from model.Binary_Classification import Model
+import numpy as np
+
+from preprocess.vectorizer import cleaner, UNK, EOS, SOS
 
 parser = argparse.ArgumentParser(description='Run experiments on a dataset')
 parser.add_argument('--dataset', type=str, required=True)
@@ -25,13 +33,32 @@ if args.output_dir is not None:
 dataset.diversity = args.diversity
 config = configurations[args.encoder](dataset)
 latest_model = get_latest_model(os.path.join(config['training']['basepath'], config['training']['exp_dirname']))
-evaluator = Evaluator(dataset, latest_model, _type=dataset.trainer_type)
-y_pred, attentions = evaluator.evaluate(dataset.test_data, save_results=True)
+model = Model.init_from_config(latest_model, load_gen=False)
 
+text = 'you ignorant sheep need serious help for your stupidity \nhttps://www.youtube.com/watch?v=SXxHfb66ZgM'.lower()
+print(text)
+spans = list(TreebankWordTokenizer().span_tokenize(text))
+list_of_tokens = [text[i:j] if text[i:j] in dataset.vec.word2idx else UNK for (i, j) in spans]
+tokenized_spans = [None, *spans, None]
+list_of_tokens = [SOS, *list_of_tokens, EOS]
+sequences = list(map(lambda s: int(dataset.vec.word2idx[s]), list_of_tokens))
+predictions, attentions, conicity_values = model.evaluate([sequences])
+predictions = np.array(predictions)
 
 idx = 0
-sentence = dataset.vec.map2words(dataset.test_data.X[idx])
-for word, attn in zip(sentence, attentions[idx]):
-    print(word, round(attn, 4))
+print('prediction', predictions[idx])
+result = ''
+last_span = 0
+for word, attn, span in zip(list_of_tokens, attentions[idx], tokenized_spans):
+    if span is not None:
+        i, j = span
+        if i > last_span:
+            result += ' '
+        last_span = j
+        print(word, round(attn, 4), (i, j))
+        if attn > 0.2:
+            result += colored(text[i:j], 'red', attrs=['reverse', 'blink'])
+        else:
+            result += text[i:j]
 
-print('aaa')
+print(result)
